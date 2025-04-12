01928a02e471c5d9fa7457ff0e2ddd45a5f5bf69
Mahendra Nath Ponduri <140336191+21JR1A05C7@users.noreply.github.com>
2025-04-12 19:16:15 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login';
 import Register from './Components/Register';
 import Home from './Components/Home';
-import PrivateRoute from './Components/PrivateRoute' // Make sure the path is correct
-
+import SuperAdmin from './Components/Superadmin';
+import './App.css';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/home"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />            
-      </Routes>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/superadmin" element={<SuperAdmin />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
